6d040bccc20054015059ae120fc364eb2c1df093
gmontantez <gmontantez7@gmail.com>
2018-02-08 14:20:06 -0500

    Mined Mines Sample

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,12 +1,19 @@
-print "gail"
-puts "1\n2\n3\n4\n5\n6\n7\n9\n10"
-puts "1"
-puts "2"
-puts "3"
-puts "4"
-puts "5"
-puts "6"
-puts "7"
-puts "8"
-puts "9"
-puts "10"
+number = 1 
+
+100.times do 
+  if number == 5 
+  	puts "MinedMinds"
+  elsif number == 50
+  	puts "Fifty"
+  elsif number == 75
+  	puts "Seventy-five"
+  else
+
+  	
+  puts number 
+  end
+
+
+  number = number + 1 
+	
+end
