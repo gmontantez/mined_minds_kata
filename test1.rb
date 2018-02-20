11c1bb4fddcc374200103eeadce0389043806120
gmontantez <gmontantez7@gmail.com>
2018-02-20 16:01:36 -0500

    Test3

--- test1.rb
+++ test1.rb
@@ -1,3 +1,9 @@
 def minedminds(x)
-	return x
+	if x == 3
+		return "mined"
+	elsif x == 5
+		return "minds"
+	else 
+		return x
+	end
 end
--- test_in_class.rb
+++ test_in_class.rb
@@ -8,5 +8,16 @@ class Combined_counter_work_test < Minitest::Test
 	def test_1_returns_1
 		assert_equal(1,minedminds(1))
 	end
+	def test_7_returns_7
+		assert_equal(7,minedminds(7))
+	end
+
+	def test_3_returns_mined
+		assert_equal("mined",minedminds(3))
+	end
+	def test_5_returns_minds
+		assert_equal("minds",minedminds(5))
+	end
+
 end
 
