42c57f499f1a1ce7fba8b667b01e941eab966ba2
gmontantez <gmontantez7@gmail.com>
2018-02-22 14:10:02 -0500

    Testing Array 1-100 Return 5 to Minds

--- testing_array.rb
+++ testing_array.rb
@@ -11,4 +11,8 @@ class Test_array < Minitest::Test
 	def test_3_returns_mined
 		assert_equal("mined",minedminds(3))
 	end
+	def test_5_returns_minds
+		assert_equal("minds",minedminds(5))
+	end
+	
 end
