ef7b5d3885eb73b7a78db018505f69f8f917b10d
gmontantez <gmontantez7@gmail.com>
2018-02-27 13:28:39 -0500

    String Testing Exercise Off-by-One

--- string_exercise2.rb
+++ string_exercise2.rb
@@ -1,5 +1,23 @@
-def off_by_one(my_ticket, winning_ticket)
+def string(num1, num2)
+	matches = 0
+	for x in (0...num1.length)
+		if num1[x] == num2[x]	
+			matches +=1
+		end
+	end
+	
+	return matches
+	
+end
+
+def off_by_one(my_ticket, winning_array)
+	
 	close_array = []
-	close_array.push("1235")
+	winning_array.each do |element|
+		if string(my_ticket, element) == 3
+		close_array.push(element)
+		end
+
+	end
 	close_array
 end
--- string_testing_exercise2.rb
+++ string_testing_exercise2.rb
@@ -4,11 +4,36 @@ require_relative "string_exercise2.rb"
 class Addition_test < Minitest::Test
 	def test_boolean
 		assert_equal(true,true)
-	end	
+	end
+	
+	def test_integer
+		num1 = "1"
+		num2 = "1"
+		number = string(num1, num2)
+		assert_equal(1, number)
+	end
+	def test_four_numbers
+		num1 = "1234"
+		num2 = "1234"
+		number = string(num1, num2)
+		assert_equal(4, number)
+	end
+	def test_two_numbers
+		my_ticket = "2459"
+		winning_array = "2468"
+		number = string(my_ticket, winning_array)
+		assert_equal(2, number)
+	end
 
-	def test_winning_returns_4
+	def test_winning_returns_2
 		my_ticket = "1234"
-		winning_array = ["1234","1235"]
+		winning_array = ["1234","1239"]
+		number = off_by_one(my_ticket, winning_array)
+		assert_equal(["1239"], number)
+	end
+	def test_one_off_by_one
+		my_ticket = "1234"
+		winning_array = ["5447", "1235", "7998"]
 		number = off_by_one(my_ticket, winning_array)
 		assert_equal(["1235"], number)
 	end
